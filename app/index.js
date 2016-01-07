7b07f1de90db428f636db137d3bbfc97e279657b
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2016-01-07 22:58:20 +0100

    declare generator var

--- app/index.js
+++ app/index.js
@@ -3,6 +3,12 @@ var path = require('path');
 var util = require('util');
 var yeoman = require('yeoman-generator');
 
+var Generator = module.exports = function Generator(args, options) {
+  yeoman.generators.Base.apply(this, arguments);
+};
+
+util.inherits(Generator, yeoman.generators.Base);
+
 Generator.prototype.frontsizeFiles = function frontsizeFiles() {
   this.bowerInstall('frontsize-sass', { save: true });
 };
