c6451d15eedc5eec5fd55a7b3dadbac622118e8c
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2016-01-14 22:28:46 +0100

    start adding askFor question

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 var path = require('path');
 var util = require('util');
 var yeoman = require('yeoman-generator');
+var install = '';
 
 var Generator = module.exports = function Generator(args, options) {
   yeoman.generators.Base.apply(this, arguments);
@@ -9,6 +10,29 @@ var Generator = module.exports = function Generator(args, options) {
 
 util.inherits(Generator, yeoman.generators.Base);
 
+Generator.prototype.askFor = function askFor(argument) {
+  var cb = this.async();
+  var options = ['yes', 'no'];
+  var prompts = [{
+    type: 'list',
+    name: 'install',
+    message: 'Would you like to install a frontsize theme?',
+    choices: options
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.install = props.install;
+    cb();
+  }.bind(this));
+};
+
 Generator.prototype.frontsizeFiles = function frontsizeFiles() {
   this.bowerInstall('frontsize-sass', { save: true });
+
+  if(this.install == 'yes'){
+    
+  }
+  else{
+    
+  }
 };
