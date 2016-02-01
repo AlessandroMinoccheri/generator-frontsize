a0c2c6f8637fe00fbe1a01fce4919fe10521ef1b
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2016-02-02 00:06:21 +0100

    removed function unuseful

--- app/index.js
+++ app/index.js
@@ -40,68 +40,66 @@ Generator.prototype.askFor = function askFor(argument) {
     }.bind(this));
 };
 
-function proceedAfterDownload(){
-    var directory = 'bower_components';
-    var pattern = /\/$/;
-    if (fs.existsSync('./.bowerrc')) {
-        var obj = JSON.parse(fs.readFileSync('./.bowerrc', 'utf8'));
-        if(obj.directory !== undefined){
-            directory = obj.directory;
-        }
-    }
-    directory = directory.replace(pattern, '');
-
-    //check if package.json exists in root
-    if (fs.existsSync('./package.json')) {
-        var dependenciesFrontsize = '';
-        var dependenciesRoot = '';
-        
-        var packageJsonFrontsize = JSON.parse(fs.readFileSync(directory + '/frontsize-sass/package.json', 'utf8'));
-        if(packageJsonFrontsize.dependencies !== undefined){
-            dependenciesFrontsize = packageJsonFrontsize.dependencies;
-        }
-        var packageJsonRoot = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-        if(packageJsonRoot.dependencies !== undefined){
-            dependenciesRoot = packageJsonRoot.dependencies;
-        }
-
-        if(dependenciesRoot != ''){
-            packageJsonRoot.dependencies = absorb(dependenciesRoot, dependenciesFrontsize);
-        }
-        else{
-            packageJsonRoot.dependencies = dependenciesFrontsize;
-        }
-
-        fs.writeFile('./package.json', JSON.stringify(packageJsonRoot, null, 2), function(err) {
-            if(err) {
-                return console.log(err);
-            }
-        }); 
-    }
-    else{
-        fs.createReadStream(directory + '/frontsize-sass/package.json').pipe(fs.createWriteStream('./package.json'));
-    }
-
-    //quando inserisco il path devo verificare che non esista e lo creo, se esiste gia la cartella theme al suo interno dare errore
-    if(fs.exists(this.pathInstall)){
-        return console.log('Path already exist!');
-    }
-    else{
-        //create directory
-        mkdirp(this.pathInstall);
-
-        //move files
-        //mv('vendor/frontsize-sass/themes/default/', this.pathInstall + '/', {mkdirp: true}, function(err) {
-        //    return console.log('Error moving files! ' + err);
-        //});
-    }
-}
-
 Generator.prototype.frontsizeFiles = function frontsizeFiles() {
     var here = this;
     async.parallel([
         function(){
             here.bowerInstall('frontsize-sass', { save: true });
         }
-    ], proceedAfterDownload);
+    ], function done(){
+        var directory = 'bower_components';
+        var pattern = /\/$/;
+        if (fs.existsSync('./.bowerrc')) {
+            var obj = JSON.parse(fs.readFileSync('./.bowerrc', 'utf8'));
+            if(obj.directory !== undefined){
+                directory = obj.directory;
+            }
+        }
+        directory = directory.replace(pattern, '');
+
+        //check if package.json exists in root
+        if (fs.existsSync('./package.json')) {
+            var dependenciesFrontsize = '';
+            var dependenciesRoot = '';
+            
+            var packageJsonFrontsize = JSON.parse(fs.readFileSync(directory + '/frontsize-sass/package.json', 'utf8'));
+            if(packageJsonFrontsize.dependencies !== undefined){
+                dependenciesFrontsize = packageJsonFrontsize.dependencies;
+            }
+            var packageJsonRoot = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+            if(packageJsonRoot.dependencies !== undefined){
+                dependenciesRoot = packageJsonRoot.dependencies;
+            }
+
+            if(dependenciesRoot != ''){
+                packageJsonRoot.dependencies = absorb(dependenciesRoot, dependenciesFrontsize);
+            }
+            else{
+                packageJsonRoot.dependencies = dependenciesFrontsize;
+            }
+
+            fs.writeFile('./package.json', JSON.stringify(packageJsonRoot, null, 2), function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            }); 
+        }
+        else{
+            fs.createReadStream(directory + '/frontsize-sass/package.json').pipe(fs.createWriteStream('./package.json'));
+        }
+
+        //quando inserisco il path devo verificare che non esista e lo creo, se esiste gia la cartella theme al suo interno dare errore
+        if(fs.exists(here.pathInstall)){
+            return console.log('Path already exist!');
+        }
+        else{
+            //create directory
+            mkdirp(here.pathInstall);
+
+            //move files
+            mv('vendor/frontsize-sass/themes/default/', here.pathInstall + '/', {mkdirp: true}, function(err) {
+                return console.log('Error moving files! ' + err);
+            });
+        }
+    });
 };
