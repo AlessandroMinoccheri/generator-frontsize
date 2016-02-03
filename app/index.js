6c9edd89cd5f25d9b5f5201831bdf3c13e45798e
Alessandro Minoccheri <alessandro@webanddesign.it>
2016-02-03 22:15:16 +0100

    trying fix async function

--- app/index.js
+++ app/index.js
@@ -41,69 +41,61 @@ Generator.prototype.askFor = function askFor(argument) {
 };
 
 Generator.prototype.frontsizeFiles = function frontsizeFiles() {
-    var here = this;
-    //async.parallel([
-        //function(callback){
-            console.info('download');
-            here.bowerInstall('frontsize-sass', { save: true });
-        //    callback(null, 'xyz\n');
-        //}], 
-        //function(error, results) {
-            console.info('callback');
-            var directory = 'bower_components';
-            var pattern = /\/$/;
-            if (fs.existsSync('./.bowerrc')) {
-                var obj = JSON.parse(fs.readFileSync('./.bowerrc', 'utf8'));
-                if(obj.directory !== undefined){
-                    directory = obj.directory;
-                }
-            }
-            directory = directory.replace(pattern, '');
+    console.info('download');
+    this.bowerInstall('frontsize-sass', { save: true });
+
+    var directory = 'bower_components';
+    var pattern = /\/$/;
+    if (fs.existsSync('./.bowerrc')) {
+        var obj = JSON.parse(fs.readFileSync('./.bowerrc', 'utf8'));
+        if(obj.directory !== undefined){
+            directory = obj.directory;
+        }
+    }
+    directory = directory.replace(pattern, '');
     
-            //check if package.json exists in root
-            if (fs.existsSync('./package.json')) {
-                var dependenciesFrontsize = '';
-                var dependenciesRoot = '';
-                
-                var packageJsonFrontsize = JSON.parse(fs.readFileSync(directory + '/frontsize-sass/package.json', 'utf8'));
-                if(packageJsonFrontsize.dependencies !== undefined){
-                    dependenciesFrontsize = packageJsonFrontsize.dependencies;
-                }
-                var packageJsonRoot = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-                if(packageJsonRoot.dependencies !== undefined){
-                    dependenciesRoot = packageJsonRoot.dependencies;
-                }
+    //check if package.json exists in root
+    if (fs.existsSync('./package.json')) {
+        var dependenciesFrontsize = '';
+        var dependenciesRoot = '';
+        
+        var packageJsonFrontsize = JSON.parse(fs.readFileSync(directory + '/frontsize-sass/package.json', 'utf8'));
+        if(packageJsonFrontsize.dependencies !== undefined){
+            dependenciesFrontsize = packageJsonFrontsize.dependencies;
+        }
+        var packageJsonRoot = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+        if(packageJsonRoot.dependencies !== undefined){
+            dependenciesRoot = packageJsonRoot.dependencies;
+        }
     
-                if(dependenciesRoot != ''){
-                    packageJsonRoot.dependencies = absorb(dependenciesRoot, dependenciesFrontsize);
-                }
-                else{
-                    packageJsonRoot.dependencies = dependenciesFrontsize;
-                }
+        if(dependenciesRoot != ''){
+            packageJsonRoot.dependencies = absorb(dependenciesRoot, dependenciesFrontsize);
+        }
+        else{
+            packageJsonRoot.dependencies = dependenciesFrontsize;
+        }
     
-                fs.writeFile('./package.json', JSON.stringify(packageJsonRoot, null, 2), function(err) {
-                    if(err) {
-                        return console.log(err);
-                    }
-                }); 
+        fs.writeFile('./package.json', JSON.stringify(packageJsonRoot, null, 2), function(err) {
+            if(err) {
+                return console.log(err);
             }
-            else{
-                fs.createReadStream(directory + '/frontsize-sass/package.json').pipe(fs.createWriteStream('./package.json'));
-            }
+        }); 
+    }
+    else{
+        fs.createReadStream(directory + '/frontsize-sass/package.json').pipe(fs.createWriteStream('./package.json'));
+    }
     
-            //quando inserisco il path devo verificare che non esista e lo creo, se esiste gia la cartella theme al suo interno dare errore
-            if(fs.exists(here.pathInstall)){
-                return console.log('Path already exist!');
-            }
-            else{
-                //create directory
-                mkdirp(here.pathInstall);
+    //quando inserisco il path devo verificare che non esista e lo creo, se esiste gia la cartella theme al suo interno dare errore
+    if(fs.exists(here.pathInstall)){
+        return console.log('Path already exist!');
+    }
+    else{
+        //create directory
+        mkdirp(here.pathInstall);
     
-                //move files
-                mv(directory + '/frontsize-sass/themes/default/', here.pathInstall + '/', {mkdirp: true}, function(err) {
-                    return console.log('Error moving files! ' + err);
-                });
-            }
-        //}
-    //);
+        //move files
+        mv(directory + '/frontsize-sass/themes/default/', here.pathInstall + '/', {mkdirp: true}, function(err) {
+            return console.log('Error moving files! ' + err);
+        });
+    }
 };
